edac0c7bec6ce651e4ee445e3d6c7bc8b021587f
subha <subhangvekariya064@gmail.com>
2024-06-26 04:16:55 -1200

    youtube pack

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:newsapp/demo_payment.dart';
+import 'package:newsapp/Demo/demo_payment.dart';
 import 'package:newsapp/res/consts/colors.dart';
 import 'package:newsapp/res/consts/consts.dart';
 import 'package:newsapp/res/routs/routes.dart';
+
+import 'Demo/youtube.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,15 +21,16 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: DemoPayment(),);
-    //   GetMaterialApp(
-    //   debugShowCheckedModeBanner: false,
-    //   title: AppStrings.appname,
-    //   theme: ThemeData(
-    //       fontFamily: AppFonts.regular,
-    //       scaffoldBackgroundColor: AppColors.background,
-    //       iconTheme: IconThemeData(color: AppColors.icon)),
-    //   getPages: AppRoutes.getRoutes(),
-    // );
+    return
+      // MaterialApp(home: DemoPayment(),);
+      GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: AppStrings.appname,
+      theme: ThemeData(
+          fontFamily: AppFonts.regular,
+          scaffoldBackgroundColor: AppColors.background,
+          iconTheme: IconThemeData(color: AppColors.icon)),
+      getPages: AppRoutes.getRoutes(),
+    );
   }
 }
